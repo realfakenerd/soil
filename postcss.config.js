59741bf5e1dd5d80b56070614cab4fdaafea9cfa
realfakenerd <lucas210898@hotmail.com>
2024-06-21 17:14:47 -0300

    smaller footprint

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,11 @@
 import autoprefixer from 'autoprefixer'
 import tailwindcss from 'tailwindcss'
-
+import cssnano from 'cssnano'
 
 export default {
 	plugins: {
 		tailwindcss,
-		autoprefixer
+		autoprefixer,
+		cssnano
 	}
- }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,49 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-import {browserslistToTargets} from 'lightningcss';
 import browserslist from 'browserslist';
-
+import { browserslistToTargets } from 'lightningcss';
+import { defineConfig } from 'vite';
+import viteCompression from 'vite-plugin-compression';
+import lightningcss from 'vite-plugin-lightningcss';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	css: {
-		transformer: 'lightningcss',
-		lightningcss: {
-			targets: browserslistToTargets(browserslist('>= 0.25%'))
-		}
-	},
+	plugins: [
+		sveltekit(),
+		lightningcss({
+			targets: browserslistToTargets(browserslist('last 2 versions, not dead'))
+		}),
+		viteCompression({
+			algorithm: 'brotliCompress',
+			verbose: false,
+			threshold: 512,
+			compressionOptions: {
+				level: 3
+			}
+		})
+	],
 	build: {
-		cssMinify: 'lightningcss'
+		cssMinify: 'esbuild',
+		minify: 'terser',
+		terserOptions: {
+			compress: {
+				drop_console: true,
+				arrows: true,
+				dead_code: true,
+				drop_debugger: true,
+				pure_getters: true,
+				keep_infinity: true,
+				ie8: false
+			},
+			module: true,
+			parse: {
+				html5_comments: false
+			},
+			output: {
+				comments: false
+			},
+			ecma: 2020,
+			ie8: false,
+			mangle: true,
+			safari10: false
+		}
 	}
 });
